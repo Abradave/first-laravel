e78fc306f01b67dd16a4a4176d19b1c22b4f9b77
Abradave <109905261+Abradave@users.noreply.github.com>
2024-01-12 11:01:52 +0100

    first commit

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get("/todo", function () {
+    return Todo::all();
+});
+
+Route::post("/todo", function (Request $request) {
+    $todo = Todo::create(["title" => $request->title]);
+    return $todo;
+});
